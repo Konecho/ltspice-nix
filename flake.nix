6d4c4b251b56738ab254597ab9d06442deab8984
Alexandra Østermark <alex.cramt@gmail.com>
2025-01-05 15:37:44 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "ltspice";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      wrapWine = ((import ./wrapWine.nix) {inherit pkgs;}).wrapWine;
+      installer = builtins.fetchurl {
+        url = "https://ltspice.analog.com/software/LTspice64.msi";
+        sha256 = "sha256:0dhs8aiwmr1bi9yq065ppxh0gclim8dmadqawanycf07cc5z5ab2";
+      };
+      wine = pkgs.wineWowPackages.stagingFull;
+      ltspice_bin = wrapWine {
+        wine = wine;
+        name = "LTspice";
+        is64bits = true;
+        executable = "$WINEPREFIX/drive_c/Program Files/ADI/LTSpice/LTSpice.exe";
+        firstrunScript = ''
+          pushd "$WINEPREFIX/drive_c"
+            ${wine}/bin/wine msiexec /q /i ${installer}
+          popd
+        '';
+      };
+      ltspice_desktop = pkgs.makeDesktopItem {
+        name = "LTspice";
+        desktopName = "LTspice";
+        type = "Application";
+        exec = "${ltspice_bin}/bin/LTspice";
+      };
+      ltspice = pkgs.symlinkJoin {
+        name = "LTspice";
+        paths = [
+          ltspice_bin
+          ltspice_desktop
+        ];
+      };
+    in {
+      packages = {
+        ltspice = ltspice;
+        default = ltspice;
+      };
+    });
+}
